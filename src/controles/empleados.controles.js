b87f4dcf8317253da9e21e67cb201418e17a949a
Leonel Amaya <omar.mejia2099@gmail.com>
2023-04-20 22:58:59 -0500

    Creando el obtener todos los empleados

--- src/controles/empleados.controles.js
+++ src/controles/empleados.controles.js
@@ -1,6 +1,15 @@
 const {pool} = require('../database');
 
-
+export const obtenerEmpleados = async (req, res) => {
+    try {
+        const [rows] = await pool.query('SELECT * FROM empleados');
+        res.json(rows);
+    } catch (error) {
+        return res.status(500).json({
+            message: 'Algo ha ido mal'
+        })
+    }
+}
 
 
 
