1ce12fab3aaff73919e9959ad6067baad50c1835
Leonel Amaya <omar.mejia2099@gmail.com>
2023-04-24 23:04:51 -0500

    Creando metodo GET del crud, falta actualizacion

--- src/rutas/empleados.js
+++ src/rutas/empleados.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const pool = require('../database');
+const getDatos = require('c:/Users/Leonel Amaya/Documents/Dev/projects/proyecto-try-catch/team-1/src/controles/control');
 
 router.get('/as', (req, res) => {
   res.render('empleados/prueba');
@@ -14,6 +15,7 @@ router.get('/datosingreso', (req, res) => {
   res.render('empleados/datosingreso');
 });
 
+router.get('/datos', getDatos);
 
 // router.get('/', (req, res) => {
 //   res.send('asas');
