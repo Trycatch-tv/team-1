dfc681341232edb814f0d5fc52cf3bcb4acffbe4
rossillpaucar <104596532+rossillpaucar@users.noreply.github.com>
2023-04-27 23:11:01 -0500

    ADD: Se agrego la pagina de lista de empleado y colaboradores

--- src/rutas/empleados.js
+++ src/rutas/empleados.js
@@ -14,6 +14,21 @@ router.get('/datosingreso', (req, res) => {
   res.render('empleados/datosingreso');
 });
 
+router.get('/lista', (req, res) => {
+  const list = [  {    "id": 1,    "nombres": "Juan Carlos",    "apellidos": "García Pérez",    "cargo": "Programador",    "estado": 1  },  
+                  {    "id": 2,    "nombres": "María Fernanda",    "apellidos": "González Ortiz",    "cargo": "Diseñadora",    "estado": 0  },  
+                  {    "id": 3,    "nombres": "Carlos Andrés",    "apellidos": "Martínez Suárez",    "cargo": "Gerente",    "estado": 1  },  
+                  {    "id": 4,    "nombres": "Laura Andrea",    "apellidos": "Castro Sánchez",    "cargo": "Contadora",    "estado": 0  },  
+                  {    "id": 5,    "nombres": "Santiago Alejandro",    "apellidos": "Rojas Ramírez",    "cargo": "Ingeniero",    "estado": 1  },  
+                  {    "id": 6,    "nombres": "Ana Sofía",    "apellidos": "Hernández González",    "cargo": "Marketing",    "estado": 0  },  
+                  {    "id": 7,    "nombres": "Juan David",    "apellidos": "Herrera Gutiérrez",    "cargo": "Programador",    "estado": 1  },  
+                  {    "id": 8,    "nombres": "Paola Alejandra",    "apellidos": "Ortiz Pérez",    "cargo": "Diseñadora",    "estado": 0  },  
+                  {    "id": 9,    "nombres": "Jorge Alberto",    "apellidos": "Ramírez Castro",    "cargo": "Gerente",    "estado": 1  },  
+                  {    "id": 10,    "nombres": "Isabela Sofía",    "apellidos": "López Hernández",    "cargo": "Contadora",    "estado": 0  }];
+
+
+  res.render('empleados/lista',{list});
+});
 
 // router.get('/', (req, res) => {
 //   res.send('asas');
