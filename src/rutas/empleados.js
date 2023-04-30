6747d4e489a8ea4639a38c729398c41c6416fe92
Leonel Amaya <omar.mejia2099@gmail.com>
2023-04-30 18:14:39 -0500

    Crud proyectos terminado

--- src/rutas/empleados.js
+++ src/rutas/empleados.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const pool = require('../database');
-const getDatos = require('c:/Users/Leonel Amaya/Documents/Dev/projects/proyecto-try-catch/team-1/src/controles/control');
 
 router.get('/as', (req, res) => {
   res.render('empleados/prueba');
@@ -15,7 +14,76 @@ router.get('/datosingreso', (req, res) => {
   res.render('empleados/datosingreso');
 });
 
-router.get('/datos', getDatos);
+//Metodo para traer los proyectos de la base de datos
+router.get('/datos', async (req,res) => {
+  try {
+    const result = await pool.query('SELECT * FROM datos_ingreso')
+    res.send(result)  
+  } catch (error) {
+    return res.status(500).json({
+      message: 'Algo ha ido mal'
+  })
+  }
+});
+
+//Metodo para ingresar los proyectos
+router.post('/ingresodatos', async (req, res) => {
+  const {cargo, departamento, proyecto, sueldo} = req.body;
+  console.log(cargo, departamento, proyecto, sueldo);
+  
+  try {
+    const rows = await pool.query('INSERT INTO datos_ingreso(cargo, departamento, proyecto, sueldo) VALUES (?, ?, ?, ?)', [cargo, departamento, proyecto, sueldo]);
+    return res.send({
+      idDato: rows.insertId,
+      cargo,
+      departamento,
+      proyecto,
+      sueldo
+    })
+  } catch (error) {
+    return res.status(500).json({
+      message: 'Algo ha ido mal'
+    })
+  }
+});
+
+//Metodo para eliminar proyectos
+router.delete('/eliminar/:id', async (req, res) => {
+  try {
+    const result = await pool.query('DELETE FROM datos_ingreso WHERE idDato = ?', [req.params.id])
+    console.log(result);
+
+    if(result.affectedRows <= 0) return res.status(404).json({
+      message: "No encontrado"
+    })
+
+    res.sendStatus(204)
+  } catch (error) {
+    res.status(500).json({
+      message: 'Algo ha ido mal'
+    })
+  }
+})
+
+//Metodo para actualizar los proyectos
+router.patch('/actualizar/:id', async (req, res) => {
+  const {id} = req.params
+  const {cargo, departamento, proyecto, sueldo} = req.body
+
+  try {
+    const result = await pool.query('UPDATE datos_ingreso SET cargo = IFNULL(?, cargo), departamento = IFNULL(?, departamento), proyecto = IFNULL(?, proyecto), sueldo = IFNULL(?, sueldo) WHERE idDato = ?', [cargo, departamento, proyecto, sueldo, id])
+
+    if(result.affectedRows === 0) return res.status(404).json({message: 'No encontrado'})
+
+    const rows = await pool.query('SELECT * FROM datos_ingreso WHERE idDato = ?', [id])
+    console.log(result);
+    res.json(rows[0])
+  } catch (error) {
+    res.status(500).json({
+      message: 'Algo ha ido mal'
+    })
+  }
+})
 
 // router.get('/', (req, res) => {
 //   res.send('asas');
