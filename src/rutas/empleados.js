c534e2e8c183cd199340ddb54992f5b707141639
mispuri <charrychurry@gmail.com>
2023-05-01 13:22:08 -0500

    cambios

--- src/rutas/empleados.js
+++ src/rutas/empleados.js
@@ -15,75 +15,89 @@ router.get('/datosingreso', (req, res) => {
 });
 
 //Metodo para traer los proyectos de la base de datos
-router.get('/datos', async (req,res) => {
+router.get('/datos', async (req, res) => {
   try {
-    const result = await pool.query('SELECT * FROM datos_ingreso')
-    res.send(result)  
+    const result = await pool.query('SELECT * FROM datos_ingreso');
+    res.send(result);
   } catch (error) {
     return res.status(500).json({
-      message: 'Algo ha ido mal'
-  })
+      message: 'Algo ha ido mal',
+    });
   }
 });
 
 //Metodo para ingresar los proyectos
 router.post('/ingresodatos', async (req, res) => {
-  const {cargo, departamento, proyecto, sueldo} = req.body;
+  const { cargo, departamento, proyecto, sueldo } = req.body;
   console.log(cargo, departamento, proyecto, sueldo);
-  
+
   try {
-    const rows = await pool.query('INSERT INTO datos_ingreso(cargo, departamento, proyecto, sueldo) VALUES (?, ?, ?, ?)', [cargo, departamento, proyecto, sueldo]);
+    const rows = await pool.query(
+      'INSERT INTO datos_ingreso(cargo, departamento, proyecto, sueldo) VALUES (?, ?, ?, ?)',
+      [cargo, departamento, proyecto, sueldo]
+    );
     return res.send({
       idDato: rows.insertId,
       cargo,
       departamento,
       proyecto,
-      sueldo
-    })
+      sueldo,
+    });
   } catch (error) {
     return res.status(500).json({
-      message: 'Algo ha ido mal'
-    })
+      message: 'Algo ha ido mal',
+    });
   }
 });
 
 //Metodo para eliminar proyectos
 router.delete('/eliminar/:id', async (req, res) => {
   try {
-    const result = await pool.query('DELETE FROM datos_ingreso WHERE idDato = ?', [req.params.id])
+    const result = await pool.query(
+      'DELETE FROM datos_ingreso WHERE idDato = ?',
+      [req.params.id]
+    );
     console.log(result);
 
-    if(result.affectedRows <= 0) return res.status(404).json({
-      message: "No encontrado"
-    })
+    if (result.affectedRows <= 0)
+      return res.status(404).json({
+        message: 'No encontrado',
+      });
 
-    res.sendStatus(204)
+    res.sendStatus(204);
   } catch (error) {
     res.status(500).json({
-      message: 'Algo ha ido mal'
-    })
+      message: 'Algo ha ido mal',
+    });
   }
-})
+});
 
 //Metodo para actualizar los proyectos
 router.patch('/actualizar/:id', async (req, res) => {
-  const {id} = req.params
-  const {cargo, departamento, proyecto, sueldo} = req.body
+  const { id } = req.params;
+  const { cargo, departamento, proyecto, sueldo } = req.body;
 
   try {
-    const result = await pool.query('UPDATE datos_ingreso SET cargo = IFNULL(?, cargo), departamento = IFNULL(?, departamento), proyecto = IFNULL(?, proyecto), sueldo = IFNULL(?, sueldo) WHERE idDato = ?', [cargo, departamento, proyecto, sueldo, id])
+    const result = await pool.query(
+      'UPDATE datos_ingreso SET cargo = IFNULL(?, cargo), departamento = IFNULL(?, departamento), proyecto = IFNULL(?, proyecto), sueldo = IFNULL(?, sueldo) WHERE idDato = ?',
+      [cargo, departamento, proyecto, sueldo, id]
+    );
 
-    if(result.affectedRows === 0) return res.status(404).json({message: 'No encontrado'})
+    if (result.affectedRows === 0)
+      return res.status(404).json({ message: 'No encontrado' });
 
-    const rows = await pool.query('SELECT * FROM datos_ingreso WHERE idDato = ?', [id])
+    const rows = await pool.query(
+      'SELECT * FROM datos_ingreso WHERE idDato = ?',
+      [id]
+    );
     console.log(result);
-    res.json(rows[0])
+    res.json(rows[0]);
   } catch (error) {
     res.status(500).json({
-      message: 'Algo ha ido mal'
-    })
+      message: 'Algo ha ido mal',
+    });
   }
-})
+});
 
 // router.get('/', (req, res) => {
 //   res.send('asas');
